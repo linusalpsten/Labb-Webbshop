692a15a632e71475ab9acc9da82c3d3179b41060
Linus Alpsten <linus.alpsten@telia.com>
2020-05-06 16:21:51 +0200

    add imagepath to item

--- Labb Webshop/Models/Item.cs
+++ Labb Webshop/Models/Item.cs
@@ -11,5 +11,6 @@ namespace Labb_Webshop.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        public string ImagePath { get; set; }
     }
 }
