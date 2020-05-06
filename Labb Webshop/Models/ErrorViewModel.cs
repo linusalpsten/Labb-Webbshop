f690f6da306eeb7427b70bae7bbb0a58a032a198
Linus Alpsten <linus.alpsten@telia.com>
2020-05-06 15:07:00 +0200

    Add project files.

---
+++ Labb Webshop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Labb_Webshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
