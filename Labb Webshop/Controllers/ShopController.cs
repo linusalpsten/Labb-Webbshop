6935cc1f1e4d2241bd7487df8b90439a6b1533a6
Linus Alpsten <linus.alpsten@telia.com>
2020-05-06 16:22:22 +0200

    shopcontroller and item list

---
+++ Labb Webshop/Controllers/ShopController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Labb_Webshop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Labb_Webshop.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            var list = new List<Item>()
+            {
+                new Item
+                {
+                    Name = "TestItem",
+                    Description = "Item for testing",
+                    Price = 10m,
+                    ImagePath = "/default.png"
+                }
+            };
+            return View(list);
+        }
+
+        public IActionResult AddToCard(Guid Id)
+        {
+            var cart = Request.Cookies.SingleOrDefault(c => c.Key == "cart");
+            string cartContent = "";
+
+            if (cart.Value != null)
+            {
+                cartContent = cart.Value;
+                cartContent += "," + Id;
+            }
+            else
+            {
+                cartContent += Id;
+            }
+
+            Response.Cookies.Append("cart", cartContent);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
