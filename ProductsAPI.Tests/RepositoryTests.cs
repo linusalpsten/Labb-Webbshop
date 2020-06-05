347ad3349430ae72a8eb7a607eae2626581a97c5
Linus Alpsten <linus.alpsten@telia.com>
2020-06-05 16:03:22 +0200

    Add products API and tests

---
+++ ProductsAPI.Tests/RepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using ProductsAPI.Models;
+using ProductsAPI.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProductsAPI.Tests
+{
+    public class RepositoryTests
+    {
+        [Fact]
+        public void GetProductById_Returns_Product()
+        {
+            var id = Guid.Empty;
+            var productsRepository = new ProductsRepository();
+            var product = productsRepository.GetById(id);
+
+            Assert.IsType<Product>(product);
+            Assert.Equal(id, product.Id);
+        }
+    }
+}
---
+++ ProductsAPI/Repository/ProductsRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAPI.Repository
+{
+    public class ProductsRepository
+    {
+        public Product GetById(Guid id)
+        {
+            return new Product();
+        }
+    }
+}
