c80c12efa8975339ca75a185431dc05260b6f0bc
Linus Alpsten <linus.alpsten@telia.com>
2020-06-15 15:51:03 +0200

    OrdersAPI with tests

---
+++ OrdersAPI/Controllers/OrderController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OrdersAPI.Models;
+using OrdersAPI.Repository;
+
+namespace OrdersAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        public OrderController()
+        {
+        }
+
+        [HttpGet]
+        [Route("[controller]/GetAll")]
+        public IEnumerable<Order> GetAll()
+        {
+            return new OrdersRepository().GetAll();
+        }
+
+        [HttpGet]
+        [Route("[controller]/GetById/{id}")]
+        public Order GetById(Guid id)
+        {
+            return new OrdersRepository().GetById(id);
+        }
+
+        [HttpGet]
+        [Route("[controller]/GetByUserId/{id}")]
+        public IEnumerable<Order> GetByUserId(Guid userId)
+        {
+            return new OrdersRepository().GetByUserId(userId);
+        }
+
+        [HttpGet]
+        [Route("[controller]/GetByProductId/{id}")]
+        public IEnumerable<Order> GetByProductId(Guid userId)
+        {
+            return new OrdersRepository().GetByProductId(userId);
+        }
+
+        [HttpPost]
+        [Route("[controller]/Add")]
+        public Order Add(Order order)
+        {
+            return new OrdersRepository().Add(order);
+        }
+
+        [HttpPost]
+        [Route("[controller]/RemoveById/{id}")]
+        public Order RemoveById(Guid id)
+        {
+            return new OrdersRepository().RemoveById(id);
+        }
+    }
+}
---
+++ OrdersAPI/Models/Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrdersAPI.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Amount { get; set; }
+
+        public static bool operator ==(Order o1, Order o2)
+        {
+            if (o1.Id != o2.Id)
+            {
+                return false;
+            }
+            if (o1.UserId != o2.UserId)
+            {
+                return false;
+            }
+            if (o1.ProductId != o2.ProductId)
+            {
+                return false;
+            }
+            if (o1.Amount != o2.Amount)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool operator !=(Order o1, Order o2)
+        {
+            return !(o1 == o2);
+        }
+    }
+}
